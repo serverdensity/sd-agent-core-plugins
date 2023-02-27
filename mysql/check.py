f08d75f9b516d1b65ef9f2ba29ca59040c65f7b1
Steve Hurley <15345515+shurl@users.noreply.github.com>
2023-02-27 18:07:19 +0000

    handle updated maraidb innodb status output

--- mysql/check.py
+++ mysql/check.py
@@ -1094,9 +1094,14 @@ class MySql(AgentCheck):
                     results['Innodb_pending_aio_log_ios'] = 0
                     results['Innodb_pending_aio_sync_ios'] = 0
             elif line.find('Pending flushes (fsync)') == 0:
-                # Pending flushes (fsync) log: 0; buffer pool: 0
-                results['Innodb_pending_log_flushes'] = long(row[4])
-                results['Innodb_pending_buffer_pool_flushes'] = long(row[7])
+                if len(row) == 8:
+                    # Pending flushes (fsync) log: 0; buffer pool: 0
+                    results['Innodb_pending_log_flushes'] = long(row[4])
+                    results['Innodb_pending_buffer_pool_flushes'] = long(row[7])
+                elif len(row) == 4:
+                    # Pending flushes (fsync): 0
+                    results['Innodb_pending_log_flushes'] = long(row[3])
+                    results['Innodb_pending_buffer_pool_flushes'] = long(row[3])
 
             # INSERT BUFFER AND ADAPTIVE HASH INDEX
             elif line.find('Ibuf for space 0: size ') == 0:
